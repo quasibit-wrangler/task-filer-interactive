5767ee9f1a5c71b0c7b777d45db18c96f16592ba
sleek-turtle <maddaloc@oregonstate.edu>
2018-07-03 19:12:09 -0700

    adding more to the What modal
    also some beauty fixes. and some event listeneres

--- public/scripts/modals.js
+++ public/scripts/modals.js
@@ -2,16 +2,16 @@
 dynamiccall with innerHTML.
 */
  var newTaskModal =
- '<article class="modal genModal"> \
- <button> Who? </button> \
- <button> What? </button> \
- <button> Where? </button> \
- <button> When? </button> \
+ '<article class="modal geneal"> \
+ <button class="who"> Who? </button> \
+ <button class="what"> What? </button> \
+ <button class="where"> Where? </button> \
+ <button class="when"> When? </button> \
  <br /> <br /> <br /> \
  <table> \
  <tr> \
-  <td><button id="cancel"> |x| Cancel |X| </button></td> \
-  <td><button id="submit"> Submit </button> </td>\
+  <td><button class="cancel"> |x| Cancel |X| </button></td> \
+  <td><button class="submit"> |✓| Submit |✓| </button> </td>\
   </tr> \
   </table> \
  </article>';
@@ -20,6 +20,33 @@ var whoModal = ''; //list of names also write inss
 var whatModal = ''; // picutres of material, quantity and type.
 var whereModal = ''; // maybe image map? suggestions.
 var whenModal = ''; // this is basically just a date.
+
+
+/* These are all the different things that can be done,
+when used, the cateogry dropbox is filled based on the data.
+name :: cateogry :: img
+*/
+var whatData = [{
+    name: "meow",
+    category: "meowzers",
+    img: ""
+  },
+  {
+    name: "purr",
+    category: "meowzers",
+    img: ""
+  },
+  {
+    name: "woof",
+    category: "woofers",
+    img: ""
+  },
+  {
+    name: "mlem",
+    category: "woofers",
+    img: ""
+  }
+]
 
 /* === Event listeners
 */
@@ -30,6 +57,28 @@ window.addEventListener("load",function(){
 /*=== end event listeners
 */
 
+/*name: handles click on the gernal modals
+input: click locaiton
+output: decides which modal
+*/
+function handleGeneralModal(event){
+    var name = event.target.classList[0];
+    console.log('which one was clicked?', name);
+    // most of them bring up a speicif modal so i throw them at
+    //same function
+    switch(name){
+      case "who":
+      case "what":
+      case "when":
+      case "where":
+            specificModal(name);break;
+      case "sumbit":
+      case "cancel":
+            verifyAllInfo();break;
+      default: console.log("what");break;
+    }
+    return;
+}
 
 /*Add a new task!
 This will open up the modal to the
@@ -37,6 +86,8 @@ new classes Who What Where When?
 a bunch of sub modals from here.
 */
 function taskModal(){
-  document.body.querySelector('#modals').innerHTML = newTaskModal;
+  let section = document.body.querySelector('#modals');
+  section.innerHTML = newTaskModal;
+  section.addEventListener('click',handleGeneralModal);
 
 }
